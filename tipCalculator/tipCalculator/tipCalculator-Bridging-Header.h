840457789e7d6be5925f747b51dcf2ce9b03f580
Chongwei <lewgreat@gmail.com>
2016-01-28 23:37:52 +0800

    Pods installed.

---
+++ tipCalculator/tipCalculator/tipCalculator-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  tipCalculator-Bridging-Header.h
+//  tipCalculator
+//
+//  Created by Liu's MBA on 1/28/16.
+//  Copyright © 2016 dancite. All rights reserved.
+//
+
+#ifndef tipCalculator_Bridging_Header_h
+#define tipCalculator_Bridging_Header_h
+
+
+#endif /* tipCalculator_Bridging_Header_h */
+
+
+#import <AVOSCloud/AVOSCloud.h>
