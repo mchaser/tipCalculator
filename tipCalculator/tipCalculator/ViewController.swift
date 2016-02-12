bea975fcb7bfa7990f475ec91cddabc6bd3f6005
Chongwei <lewgreat@gmail.com>
2016-02-12 23:23:59 +0800

    Model View Controller.

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -22,42 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    var billAmount: Double = 0
-    var numberOfPeopleToSplit: Int = 1
-    var percentageOfTip: Double = 0.15
-    var amountOfTip:Double = 0
-    var grandTotal: Double = 0
-    var amountPerPerson: Double = 0
-    
-    var displayPercentage: Int = 0
-    func displayPercentageCal() -> Int {
-        displayPercentage = Int(percentageOfTip * Double(100))
-        return displayPercentage
-    }
-    
-    func tipCalculate(){
-        amountOfTip = billAmount * percentageOfTip
-        grandTotal = billAmount + amountOfTip
-        amountPerPerson = grandTotal / Double(numberOfPeopleToSplit)
-    }
-    
-    func displayAmountPerPerson() -> Double{
-        amountPerPerson = grandTotal / Double(numberOfPeopleToSplit)
-        return amountPerPerson
-    }
-    
     
 
     @IBOutlet weak var amountOfBill: UITextField!
 
 
-    @IBOutlet weak var totalAmount: UILabel!{
-        self.totalAmount.text! = "\(grandTotal)"
-    }
-
     
     
-
     
 }
-
---
+++ tipCalculator/tipCalculator/tipCalculatorBrain.swift
@@ -0,0 +1,36 @@
+//
+//  tipCalculatorBrain.swift
+//  tipCalculator
+//
+//  Created by Liu's MBA on 2/12/16.
+//  Copyright © 2016 dancite. All rights reserved.
+//
+
+import Foundation
+
+class tipCalBrain {
+    var billAmount: Double = 0
+    var numberOfPeopleToSplit: Int = 1
+    var percentageOfTip: Double = 0.15
+    var amountOfTip:Double = 0
+    var grandTotal: Double = 0
+    var amountPerPerson: Double = 0
+    
+    var displayPercentage: Int = 0
+    func displayPercentageCal() -> Int {
+        displayPercentage = Int(percentageOfTip * Double(100))
+        return displayPercentage
+    }
+    
+    private func tipCalculate(){
+        amountOfTip = billAmount * percentageOfTip
+        grandTotal = billAmount + amountOfTip
+        amountPerPerson = grandTotal / Double(numberOfPeopleToSplit)
+    }
+    
+    private func displayAmountPerPerson() -> Double{
+        amountPerPerson = grandTotal / Double(numberOfPeopleToSplit)
+        return amountPerPerson
+    }
+
+}
