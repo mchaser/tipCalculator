7cbc92cbe75912c2d03368ba34bae21bf50bf043
Chongwei <lewgreat@gmail.com>
2016-02-22 22:49:50 +0800

    Reading.

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
 
 
     @IBOutlet weak var amountOfBill: UITextField!
